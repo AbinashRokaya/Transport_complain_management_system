640db993379a7d71d4228f679d1331590113e41a
Abirokaya <rokayaabinash9@gmail.com>
2026-03-04 05:32:19 +0545

    database url

--- Database/database.py
+++ Database/database.py
@@ -3,8 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,Session
 from typing import Annotated
 from fastapi import Depends
+import os
 
-DATABASE_URL="postgresql://postgres:abinash@localhost:5432/transport"
+# DATABASE_URL="postgresql://postgres:abinash@localhost:5432/transport"
+DATABASE_URL = os.getenv("DATABASE_URL", "").replace(
+    "postgres://", "postgresql://"  # Render sometimes returns "postgres://"
+)
 
 engine=create_engine(DATABASE_URL)
 sessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
